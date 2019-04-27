99d9631ab960978d4584798501dadc4ee92d3ccf
Kat B <katreesebarnes@gmail.com>
2019-04-27 13:07:36 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in low_carb-cli.gemspec
+gemspec
---
+++ lib/low_carb/cli.rb
@@ -0,0 +1,8 @@
+require "low_carb/cli/version"
+
+module LowCarb
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
