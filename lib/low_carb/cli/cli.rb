88aad2168d2134e5fc1ee253343a7cd8856caa19
Kat B <katreesebarnes@gmail.com>
2019-04-29 00:11:57 +0000

    added data to cli.rb

--- lib/low_carb/cli/cli.rb
+++ lib/low_carb/cli/cli.rb
@@ -2,8 +2,17 @@
 class LowCarb::CLI
 
 def class
-puts "Best Low Carb Recipes:"
-end
 
+list_recipes
+menu
+end
+def list_recipes
+  puts "Best Low Carb Recipes:"
+  puts "1. Easy Dijon Chicken 2. St Louis Ribs"
+  
+end
+def menu
+  puts "Enter the number of the recipe you want to make:"
+end
 
 end
