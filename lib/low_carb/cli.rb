f55a8ad9dea70fed2c3a9e4ad87080a2d77360cd
Kat B <katreesebarnes@gmail.com>
2019-05-01 20:45:42 +0000

    added code to cli

--- lib/low_carb/cli.rb
+++ lib/low_carb/cli.rb
@@ -1,18 +1,10 @@
 #CLI Controller
 class LowCarb::CLI
 
-def class
-
-list_recipes
-menu
-end
-def list_recipes
-  puts "Best Low Carb Recipes:"
-  puts "1. Easy Dijon Chicken 2. St Louis Ribs"
+  def start 
+      LowCarb::Obj.get_data
+      puts LowCarb::Obj.all
   
-end
-def menu
-  puts "Enter the number of the recipe you want to make:"
-end
-
+  end
+  
 end
