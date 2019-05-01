e3d4724c436beddff5ab91343526b7734adc2a6c
Kat B <katreesebarnes@gmail.com>
2019-05-01 20:32:16 +0000

    added require to lib

--- lib/low_carb.rb
+++ lib/low_carb.rb
@@ -1,9 +1,17 @@
-require "low_carb/cli/version"
-require_relative './low_carb-cli'
+require "nokogiri"
+require 'open-url'
 
-module LowCarb
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
+require_relative "low_carb/version"
+require_relative "low_carb/low_carb_obj"
+require_relative "low_carb/cli"
+
+
+
+
+
+# module LowCarb
+#   module Cli
+#     class Error < StandardError; end
+#     # Your code goes here...
+#   end
+# end
